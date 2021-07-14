2903b67520feb85ff3b215186fb4b12e9b7a8bdc
Pranaya Anargya <panargya@gmail.com>
2021-07-14 14:44:28 +1000

    added modal into info icon

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,11 @@
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
 import { makeStyles } from "@material-ui/core";
-import { BsInfoCircle } from 'react-icons/bs'
+import { BsInfoCircle } from 'react-icons/bs';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 const navStyle = makeStyles((theme) => {
     return {
         root: {
@@ -18,8 +22,20 @@ const navStyle = makeStyles((theme) => {
             background: theme.palette.primary.light
         },
         infoIcon: {
-            fontSize: '1.5rem'
-        }
+            fontSize: '1.5rem',
+            cursor: 'pointer'
+        },
+        modal: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        paper: {
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
     }
 })
 
@@ -27,11 +43,48 @@ export default function Navigation() {
 
     const classes = navStyle() // to use the styles of makestyle() from material-ui
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const IconContainer = () => {
         return (
             <div>
-                <BsInfoCircle className={classes.infoIcon} />
+                <div
+                    onClick={() => {
+                        handleOpen()
+                        console.log('Info clicked')
+                    }}
+                >
+                    <BsInfoCircle className={classes.infoIcon} />
+                </div>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                        <div className={classes.paper}>
+                            <h2 id="transition-modal-title">Transition modal</h2>
+                            <p id="transition-modal-description">react-transition-group animates me.</p>
+                        </div>
+                    </Fade>
+                </Modal>
             </div>
+
         )
     }
 
